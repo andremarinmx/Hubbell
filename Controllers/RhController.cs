d364acc99cce13f0b62b89b666a494f8de749d37
andremarinmx <andre707.am@gmail.com>
2022-09-16 19:48:56 -0600

    Creación de modelos y vista de AgregarEmpleadoMes

---
+++ Controllers/RhController.cs
@@ -0,0 +1,43 @@
+﻿using Hubbell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hubbell.Controllers
+{
+    public class RhController : Controller
+    {
+        [HttpGet]
+        public ActionResult EmpleadosMes()
+        {
+            using (HubbellContext db = new HubbellContext())
+            {   
+                var empleados = db.EmpleadosMes.OrderBy(x => x.NumReloj).ToList();
+                return View(empleados);
+            }
+              
+        }
+
+        [HttpGet]
+        public ActionResult AgregarEmpleado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarEmpleado(int numReloj, DateTime fecha)
+        {
+            using (HubbellContext db = new HubbellContext())
+            {
+                EmpleadosMes empleadoMes = new EmpleadosMes();
+                empleadoMes.NumReloj = numReloj;
+                empleadoMes.Fecha = fecha;
+                db.EmpleadosMes.Add(empleadoMes);
+                db.SaveChanges();
+                return View("EmpleadosMes");
+            }
+        }
+    }
+}
